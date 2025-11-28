df10c456c9f6bc56769a71883938296d1fefb7cd
mahmoudessamdev <saraessan3@gmail.com>
2025-11-28 20:19:25 +0200

    feat: create cafe management

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,8 @@ import AdminDashboard from './pages/AdminDashboard';
 import EditRoomSchedule from './pages/EditRoomSchedule';
 import NotFound from './pages/NotFound';
 import { useEffect, useState } from 'react';
-
+import AdminLogin from './pages/AdminLogin';
+import CafeManagement from './pages/CafeManagement';
 const AppContent = () => {
   const location = useLocation();
   const [isAdmin, setIsAdmin] = useState<boolean>(true);
@@ -33,7 +34,13 @@ const AppContent = () => {
       location.pathname === '/analytics' ||
       location.pathname === '/room-availability' ||
       location.pathname === '/access-history' ||
-      location.pathname === '/edit-room-schedule'
+      location.pathname === '/edit-room-schedule'||
+      location.pathname === '/admin-login'||
+      location.pathname === '/gallery-management'||
+      location.pathname === '/cafe-management'||
+      location.pathname === '/booking-management'||
+      location.pathname === '/about-management'||
+      location.pathname === '/messages'
     ) {
       setIsAdmin(true);
     } else {
@@ -54,11 +61,13 @@ const AppContent = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         {/* =================== admin sections =======================*/}
+        <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/dashboard" element={<AdminDashboard />} />
         <Route path="/access-history" element={<AccessHistory />} />
         <Route path="/analytics" element={<Analytics />} />
         <Route path="/room-availability" element={<RoomAvailability />} />
         <Route path="/edit-room-schedule" element={<EditRoomSchedule />} />
+        <Route path='/cafe-management' element={<CafeManagement />} />
       </Routes>
       {showLayout && <Footer />}
     </>
